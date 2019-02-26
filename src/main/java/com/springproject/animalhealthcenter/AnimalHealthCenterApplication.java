2b533dcca8321bc060d7c7a64dab05273ee6f51c
Aremu Benjamen <bendayo38@gmail.com>
2019-02-26 13:14:17 -0800

    Adding Initial Data

---
+++ src/main/java/com/springproject/animalhealthcenter/AnimalHealthCenterApplication.java
@@ -0,0 +1,13 @@
+package com.springproject.animalhealthcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalHealthCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnimalHealthCenterApplication.class, args);
+    }
+
+}
