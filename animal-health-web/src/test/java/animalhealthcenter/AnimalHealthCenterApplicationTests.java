59196e57afb6b887ae3424a5e78d7c59b6c51b6c
Aremu Benjamen <bendayo38@gmail.com>
2019-03-01 11:07:31 -0800

    Adding Maven Plugin. closes #3

--- animal-health-web/src/test/java/animalhealthcenter/AnimalHealthCenterApplicationTests.java
+++ animal-health-web/src/test/java/animalhealthcenter/AnimalHealthCenterApplicationTests.java
@@ -2,11 +2,13 @@ package animalhealthcenter;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = {})
+@SpringBootConfiguration
 public class AnimalHealthCenterApplicationTests {
 
     @Test
