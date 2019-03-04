f8b36332941c06bcfa5ed0f196486de35d14f87f
Aremu Benjamen <bendayo38@gmail.com>
2019-03-04 03:42:03 -0800

    Refractoring to common interface. Closes #12

--- animal-health-center-data/src/main/java/com/springproject/animalhealthcenter/services/AnimalService.java
+++ animal-health-center-data/src/main/java/com/springproject/animalhealthcenter/services/AnimalService.java
@@ -2,12 +2,7 @@ package com.springproject.animalhealthcenter.services;
 
 import com.springproject.animalhealthcenter.model.Animal;
 
-import java.util.Set;
 
-public interface AnimalService {
-    Animal findById(Long id);
 
-    Animal save(Animal animal);
-
-    Set<Animal> findAll();
-}
+public interface AnimalService extends CrudService<Animal, Long> {
+    }
--- animal-health-center-data/src/main/java/com/springproject/animalhealthcenter/services/VetService.java
+++ animal-health-center-data/src/main/java/com/springproject/animalhealthcenter/services/VetService.java
@@ -2,13 +2,9 @@ package com.springproject.animalhealthcenter.services;
 
 import com.springproject.animalhealthcenter.model.Vet;
 
-import java.util.Set;
 
-public interface VetService {
-    Vet findById(Long id);
 
-    Vet save(Vet vet);
+public interface VetService extends CrudService<Vet, Long> {
 
-    Set<Vet> findAll();
 
 }
