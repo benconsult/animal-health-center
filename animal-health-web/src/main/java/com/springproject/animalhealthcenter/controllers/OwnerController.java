208e5a39f0a11d3c86321be95d78cc378cc4945c
Aremu Benjamen <bendayo38@gmail.com>
2019-03-05 19:18:09 -0800

    adding owner list and updated controller. closes #17

--- animal-health-web/src/main/java/com/springproject/animalhealthcenter/controllers/OwnerController.java
+++ animal-health-web/src/main/java/com/springproject/animalhealthcenter/controllers/OwnerController.java
@@ -1,13 +1,22 @@
 package com.springproject.animalhealthcenter.controllers;
 
+import com.springproject.animalhealthcenter.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping({"/owners"})
 @Controller
 public class OwnerController {
-    @RequestMapping({"","/", "/index", "/index.html"})
-    public String owner(){
+    private final OwnerService ownerService;
 
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
+
+    @RequestMapping({"","/", "/index", "/index.html"})
+    public String owner(Model model){
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
