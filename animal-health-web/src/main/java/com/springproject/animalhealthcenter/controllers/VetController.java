121ee1256ad5e56660cb83d5b0ae2e08305d33e4
Aremu Benjamen <bendayo38@gmail.com>
2019-03-05 19:34:39 -0800

    adding vet list and updated controller. closes #16

--- animal-health-web/src/main/java/com/springproject/animalhealthcenter/controllers/VetController.java
+++ animal-health-web/src/main/java/com/springproject/animalhealthcenter/controllers/VetController.java
@@ -1,12 +1,21 @@
 package com.springproject.animalhealthcenter.controllers;
 
+import com.springproject.animalhealthcenter.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets", "/vets/index.html"})
-    public String vet(){
+    public String vet(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
