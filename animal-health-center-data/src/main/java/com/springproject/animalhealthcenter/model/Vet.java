47a951a4e8130ea5785ac33aa959e73933eec4e2
Aremu Benjamen <bendayo38@gmail.com>
2019-03-11 05:44:40 -0700

    added speciality, added to vet. closes #28

--- animal-health-center-data/src/main/java/com/springproject/animalhealthcenter/model/Vet.java
+++ animal-health-center-data/src/main/java/com/springproject/animalhealthcenter/model/Vet.java
@@ -1,4 +1,15 @@
 package com.springproject.animalhealthcenter.model;
 
+import java.util.Set;
+
 public class Vet extends Person {
+    private Set<Speciality> specialities;
+
+    public Set<Speciality> getSpeciality() {
+        return specialities;
+    }
+
+    public void setSpeciality(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
 }
