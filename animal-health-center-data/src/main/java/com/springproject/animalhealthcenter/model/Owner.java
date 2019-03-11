070da9641c7079ccfdcfa199b14991edb4fa750c
Aremu Benjamen <bendayo38@gmail.com>
2019-03-11 05:56:59 -0700

    added contact properties. closes #29

--- animal-health-center-data/src/main/java/com/springproject/animalhealthcenter/model/Owner.java
+++ animal-health-center-data/src/main/java/com/springproject/animalhealthcenter/model/Owner.java
@@ -3,13 +3,40 @@ package com.springproject.animalhealthcenter.model;
 import java.util.Set;
 
 public class Owner extends Person {
-  private Set<Animal> animals;
+    private String address;
+    private String city;
+    private String telephone;
+    private Set<Animal> animals;
 
-    public Set<Animal> getAnimal() {
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public Set<Animal> getAnimals() {
         return animals;
     }
 
-    public void setAnimal(Set<Animal> animals) {
+    public void setAnimals(Set<Animal> animals) {
         this.animals = animals;
     }
 }
